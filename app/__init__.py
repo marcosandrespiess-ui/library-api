252df61c02466291fa6db648b530c7842294e583
Marcos André Spiess <marcosandrespiess@gmail.com>
2026-04-16 16:03:56 -0300

    feat: adiciona variáveis de ambiente com dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,15 @@ from flask import Flask
 from app.models.livro import db
 from app.routes.livro_routes import livro_bp
 from app.errors import registrar_erros
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///livros.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///livros.db")
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "chave-padrao")
     app.config["PROPAGATE_EXCEPTIONS"] = False  
     
     db.init_app(app)
@@ -15,5 +20,5 @@ def create_app():
     
     app.register_blueprint(livro_bp)
     registrar_erros(app)
-    
+
     return app
